9133881e8bfe7e1d5656b4443a18ad849459b901
Sean Wedig <sean.wedig@gmail.com>
2016-09-17 13:55:48 -0400

    Open the exercise as part of the specs

--- spec/lab2/lab_2.rb
+++ spec/lab2/lab_2.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/exercise_loader.rb'
 describe 'Lab2' do
   let(:temp_dir) { "#{ENV['TMP']}/grading/lab2" }
   let(:loader) { Grading::ExerciseLoader.new("#{ENV['USERPROFILE']}/Downloads") }
-  let!(:student_prog_path) { loader.built_exe_path_for_sln_in(temp_dir) }
+  let!(:student_prog_path) { @prog ||= loader.built_exe_path_for_sln_in(temp_dir) }
 
   before do
     FileUtils.mkdir_p(temp_dir)
@@ -24,14 +24,11 @@ describe 'Lab2' do
 
     before do
       `start "REFERENCE" "#{File.expand_path(reference_prog)}"`
+      loader.open_exercise(temp_dir)
     end
 
     it 'starts up alongside the reference app' do
       `start "STUDENT" "#{File.expand_path(student_prog_path)}"`
-    end
-
-    xit 'can be opened' do
-      loader.open_exercise(temp_dir)
     end
   end
 end
