2f7426cc0ddc32559ee5533d2728a89c7ca608ca
Sean Wedig <sean.wedig@gmail.com>
2016-10-01 13:12:14 -0400

    Update the script to build the program from the command-line and prompt to run it multiple times

--- grade_most_recent.rb
+++ grade_most_recent.rb
@@ -2,24 +2,37 @@ require 'fileutils'
 require 'find'
 require_relative 'lib/exercise_loader.rb'
 
-TEMP_DIR = "#{ENV['TMP']}/grading" 
+TEMP_DIR = "#{ENV['TMP']}/grading/lab2" 
 
-unpacker = Grading::ExerciseLoader.new("#{ENV['USERPROFILE']}/Downloads")
+loader = Grading::ExerciseLoader.new("#{ENV['USERPROFILE']}/Downloads")
 
 begin
-  most_recent = unpacker.most_recent_zip
-  unpacker.unzip_to_path(most_recent, TEMP_DIR)
+  most_recent = loader.most_recent_zip
+  loader.unzip_to_path(most_recent, TEMP_DIR)
 
+
+  puts "Building the solution..."
+  built_prog_path = loader.built_exe_path_for_sln_in(TEMP_DIR)
   puts "Opening eval and solution files"
-  unpacker.open_exercise(TEMP_DIR)
+  loader.open_exercise(TEMP_DIR)
 
+  puts "\n\nRunning it..."
+  run_it = true
+  while run_it do
+    `start "STUDENT" "#{File.expand_path(built_prog_path)}"`
+    puts "Run it again? Y/N"
+    run_it = gets.chomp.downcase == 'y'
+  end
+
+  puts "\n\nGRADING #{built_prog_path}"
   puts "\n\n...Waiting for gradin'. Hit any key to continue."
-rescue
+rescue Exception => e
   puts "NOOOOO, FAILURE"
+  puts e
 ensure 
   gets.chomp
-  unpacker.cleanup(TEMP_DIR)
+  loader.cleanup(TEMP_DIR)
 
   puts "\nCleanup downloaded file '#{most_recent}'? Y/N"
-  unpacker.cleanup(most_recent) if gets.chomp.downcase == 'y'
+  loader.cleanup(most_recent) if gets.chomp.downcase == 'y'
 end
